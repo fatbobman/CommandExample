f423ef7f8fb3a9711ccc6c31769cc647c73ac70d
东坡肘子 <55673881+fatbobman@users.noreply.github.com>
2020-06-30 11:09:44 +0800

    Initial Commit

---
+++ Shared/CommandExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommandExampleApp.swift
+//  Shared
+//
+//  Created by Yang Xu on 2020/6/30.
+//
+
+import SwiftUI
+
+@main
+struct CommandExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
